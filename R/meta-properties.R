449a2ea364bda4154545749ffb1c78b4182f7395
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-08-31 11:49:36 +0800

    feat: add `add_meta_properties()`

--- R/meta-properties.R
+++ R/meta-properties.R
@@ -38,3 +38,39 @@ get_meta_properties <- function(glycans, mp_fns = all_mp_fns()) {
   mp_fns <- purrr::map(mp_fns, rlang::as_function)
   tibble::as_tibble(purrr::map(mp_fns, ~ .x(glycans)))
 }
+
+#' Add Meta-Properties to Experiment
+#'
+#' This function adds meta-properties to the variable information of a [glyexp::experiment()].
+#' Under the hood, it uses [get_meta_properties()] to calculate the meta-properties
+#' on the "glycan_structure" column (or column specified by `struc_col`) of the variable information tibble,
+#' and then adds the result back as new columns.
+#'
+#' @param exp An [glyexp::experiment()] object.
+#' @param mp_fns A named list of meta-property functions.
+#'   Names of the list are the names of the meta-properties. Default is [all_mp_fns()].
+#'   A meta-property function should takes a `glyrepr::glycan_structure()` vector,
+#'   and returns a vector of the meta-property values.
+#'   purrr-style lambda functions are supported.
+#' @param struc_col The column name of the glycan structures in the variable information tibble.
+#'   Default is "glycan_structure".
+#'
+#' @return An [glyexp::experiment()] object with meta-properties added to the variable information.
+#' @seealso [get_meta_properties()], [glyexp::experiment()]
+#'
+#' @export
+add_meta_properties <- function(exp, mp_fns = all_mp_fns(), struc_col = "glycan_structure") {
+  checkmate::assert_class(exp, "glyexp_experiment")
+  checkmate::assert_string(struc_col)
+
+  if (!struc_col %in% colnames(exp$var_info)) {
+    cli::cli_abort(c(
+      "Column {.field {struc_col}} not found in {.field var_info}.",
+      "i" = "Got {.col {colnames(exp$var_info)}}."
+    ))
+  }
+
+  meta_properties <- get_meta_properties(exp$var_info[[struc_col]], mp_fns)
+  exp$var_info <- dplyr::bind_cols(exp$var_info, meta_properties)
+  exp
+}
