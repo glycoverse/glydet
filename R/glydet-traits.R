6be9958db4116a7a82301126bd6b1714952b6b73
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-09-09 11:40:43 +0800

    fix: fix an R CMD check note

--- R/glydet-traits.R
+++ R/glydet-traits.R
@@ -23,32 +23,32 @@
 all_traits <- function() {
   list(
     # Proportion of highmannose glycans
-    TM = prop(T == "highmannose"),
+    TM = prop(.data$T == "highmannose"),
     # Proportion of hybrid glycans
-    TH = prop(T == "hybrid"),
+    TH = prop(.data$T == "hybrid"),
     # Proportion of complex glycans
-    TC = prop(T == "complex"),
+    TC = prop(.data$T == "complex"),
     # Average number of mannoses within highmannose glycans
-    MM = wmean(nM, within = (T == "highmannose")),
+    MM = wmean(.data$nM, within = (.data$T == "highmannose")),
     # Proportion of bi-antennary glycans within complex glycans
-    CA2 = prop(nA == 2, within = (T == "complex")),
+    CA2 = prop(.data$nA == 2, within = (.data$T == "complex")),
     # Proportion of tri-antennary glycans within complex glycans
-    CA3 = prop(nA == 3, within = (T == "complex")),
+    CA3 = prop(.data$nA == 3, within = (.data$T == "complex")),
     # Proportion of tetra-antennary glycans within complex glycans
-    CA4 = prop(nA == 4, within = (T == "complex")),
+    CA4 = prop(.data$nA == 4, within = (.data$T == "complex")),
     # Proportion of fucosylated glycans
-    TF = prop((nFc + nFa) > 0),
+    TF = prop((.data$nFc + .data$nFa) > 0),
     # Proportion of core-fucosylated glycans
-    TFc = prop(nFc > 0),
+    TFc = prop(.data$nFc > 0),
     # Proportion of arm-fucosylated glycans
-    TFa = prop(nFa > 0),
+    TFa = prop(.data$nFa > 0),
     # Proportion of glycans with bisecting GlcNAc
-    TB = prop(B),
+    TB = prop(.data$B),
     # Average degree of sialylation per galactose
-    SG = wmean(nS / nG),
+    SG = wmean(.data$nS / .data$nG),
     # Average degree of galactosylation per antenna
-    GA = wmean(nG / nA),
+    GA = wmean(.data$nG / .data$nA),
     # Proportion of sialylated glycans
-    TS = prop(nS > 0)
+    TS = prop(.data$nS > 0)
   )
 }
